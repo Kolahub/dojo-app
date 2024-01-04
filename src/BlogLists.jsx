14f1bc932ab7aa3c670a07765fd51343403c179b
Ayofe Faheez <ayofefaheez@gmail.com>
2024-01-03 22:28:30 -0800

    second commit

--- src/BlogLists.jsx
+++ src/BlogLists.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from "react-router-dom";
 function BlogLists({blogs, title, /*handleDelete*/ }) {
   return (
     <div className="blogLists">
       <h2> { title } </h2>
         {blogs.map((blog) => (
         <div className="blog-preview" key = {blog.id}>
+          <Link to = {`/blogs/${blog.id}`}>
           <h2>{blog.title}</h2>
            <p>Wriiten byt { blog.author} </p>
            {/* <button onClick = {() => handleDelete(blog.id)}>Delete blog</button> */}
+           </Link>
         </div>
     ))}
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import BlogLists from './BlogLists'
-import { useState, useEffect } from 'react'
+import useFetch from './useFetch'
+
 function Home() {
-  const [blogs, setBlogs] = useState (null)
-  const [isPending, setisPending] = useState(true)
+  const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs")
   /*
   ///FUNCTION AS PROPS
   const handleDelete = (id) => {
@@ -12,17 +12,9 @@ function Home() {
     setBlogs(newBlogs)
   }
   */
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blog").then(res => {
-      return res.json();
-    }).then(data => {
-      setBlogs(data)
-      setisPending(false)
-    })
-  }, [])
   return (
     <div className="Home">
+      {error && <div> { error } </div>}
       { isPending && <div>Loading...</div> }
     {blogs && <BlogLists blogs={blogs} title = 'All Blogs' /*handleDelete = {handleDelete} */ />}
     </div>
